adb13d6a670c2a04d56c20effe4b0f0f3518c0dd
rwarren14 <rwarren@stu.jsu.edu>
2015-09-16 23:47:11 -0500

    change for acceptence.sh file. GUI build complete. RobotFramework: permission denied when running the acceptence.sh file

--- acceptance/runacceptance.sh
+++ acceptance/runacceptance.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-javac -cp .:/Users/robertwarren/GitHub/tic-tac-toe/src/main/java/edu/jsu/mcis/TicTacToe.java
-java -cp .:../build/classes/main:/Users/robertwarren/RobotFramework/robotframework-2.9.jar org.robotframework.RobotFramework TicTacToeTests.txt
-
+javac -cp .:../Users/robertwarren/GitHub/tic-tac-toe/src/main/java/edu/jsu/mcis/TicTacToeKeywords.java
+java -cp .:../Users/robertwarren/RobotFramework/swinglibrary-1.9.4.jar;../build/classes/main;/Users/robertwarren/RobotFramework/robotframework-2.9.jar org.robotframework.RobotFramework TicTacToeGUITests.txt
--- src/main/java/edu/jsu/mcis/TicTacToeGameBoard.java
+++ src/main/java/edu/jsu/mcis/TicTacToeGameBoard.java
@@ -28,11 +28,11 @@ public class TicTacToeGameBoard extends JFrame{
       mainBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       for(int i = 0; i < 3; i++){
          for(int j =0; j < 3; j++){
-            buttons[i][j] = new JButton("----");
+            buttons[i][j] = new JButton();
             buttons[i][j].setName("Location" + i + j);
             buttons[i][j].addActionListener(new ButtonListener(i,j));
             buttons[i][j].setFont(new Font("", Font.PLAIN, 55));
-            buttons[i][j].setBorderPainted(false);
+            buttons[i][j].setBorderPainted(true);
             mainBoard.add(buttons[i][j]);
             
             
@@ -60,9 +60,50 @@ public class TicTacToeGameBoard extends JFrame{
    
    private void buttonPress(int row, int col){
       Board.setMark(row, col);
-      buttons[row][col].setLabel(Board.getMark(row,col).ToString());
+      buttons[row][col].setLabel(Board.getMark(row,col).toString());
+      if (Board.winConditions() == TicTacToe.Mark.X){
+         int delay = 1000;
+         ActionListener task = 
+            new ActionListener(){
+               public void actionPerformed(ActionEvent evt){
+                  JOptionPane.showMessageDialog(null, "X is the winner!","Game over", JOptionPane.INFORMATION_MESSAGE);
+               
+               }
+               
+            };
+         Timer myTimer = new Timer(delay, task);
+         myTimer.setRepeats(false);
+         myTimer.start();
+         
+      }
       
+      else if (Board.winConditions() == TicTacToe.Mark.O){
+         int delay = 1000;
+         ActionListener task = 
+            new ActionListener(){
+               public void actionPerformed(ActionEvent evt){
+                  JOptionPane.showMessageDialog(null, "O is the winner", "Game Over", JOptionPane.INFORMATION_MESSAGE);
+               
+               }
+            };
+         Timer myTimer = new Timer(delay, task);
+         myTimer.setRepeats(false);
+         myTimer.start();
+      }
       
+      else if (Board.checkTie()){
+         int delay= 1000;
+         ActionListener task = 
+            new ActionListener(){
+               public void actionPerformed(ActionEvent evt){
+                  JOptionPane.showMessageDialog(null, "It is a tie", "Game over", JOptionPane.INFORMATION_MESSAGE);
+               
+               }
+            };
+         Timer myTimer = new Timer(delay, task);
+         myTimer.setRepeats(false);
+         myTimer.start();
+      }
    }
    
 
